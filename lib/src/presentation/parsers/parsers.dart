d35e80205500dc7e9dc265231768f60c2cafd122
Ramon Cardoso Barbosa <cardosoramonbarbosa@hotmail.com>
2021-05-26 11:24:39 -0300

    Update export

--- lib/src/presentation/parsers/parsers.dart
+++ lib/src/presentation/parsers/parsers.dart
@@ -3,6 +3,8 @@ export 'flutter_widgets/edge_insets/edge_insets_properties.dart';
 export 'flutter_widgets/expanded_parser.dart';
 export 'flutter_widgets/form/elements/wrap_form_property.dart';
 export 'flutter_widgets/form/form_parser.dart';
+export 'flutter_widgets/form/manager/default_form_manager.dart';
+export 'flutter_widgets/form/manager/form_manager.dart';
 export 'flutter_widgets/icon/utils/icons_map.dart';
 export 'flutter_widgets/icon_button/icon_button_parser.dart';
 export 'flutter_widgets/row_parser.dart';
